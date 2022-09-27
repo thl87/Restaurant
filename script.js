e04072399e6e0ff081d90550c338a7ec10a295cc
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-09-27 11:26:56 +0200

    Main page done + file structure

---
+++ script.js
@@ -0,0 +1,8 @@
+function responsiveNav() {
+    let nav = document.querySelector("ul");
+    if (nav.className === "navList") {
+        nav.classList.add("responsive");
+    } else {
+        nav.className = "navList"
+    }
+}
